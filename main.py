6d92b58f355950f9db52bbebb955635bf43e16f1
Stephen Mare <mare.stephen@gmail.com>
2020-12-21 15:53:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+pizzas = ["bbq", "chicken", "olive", "cheese", "anchovies"]
+for pizza in pizzas:
+  print (f"I love {pizza}!")
+print ("I really love pizza!")
+
+animals = ["chimpanzee", "orangutan", "babboon", "gibbon"]
+for monkey in animals:
+  print (f"A {monkey} would make a great pet.")
+
+squares = []
+for value in range (1, 11):
+  squares.append(value ** 2)
+print (squares)
